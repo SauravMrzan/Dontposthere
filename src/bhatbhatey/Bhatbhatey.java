b96b9e4061d4b8702e4f23788a6f86f0d108c022
sthas <sthasantosh070@gmail.com>
2024-08-15 17:21:15 +0545

    Added

---
+++ src/bhatbhatey/Bhatbhatey.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bhatbhatey;
+
+import bhatbhatey.View.Newlogin;
+import bhatbhatey.View.Register;
+
+
+/**
+ *
+ * @author sthas
+ */
+public class Bhatbhatey {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Startscreen ss = new Startscreen();
+//        ss.setVisible(true);
+        
+        Register ns = new Register();
+        ns.setVisible(true);
+        
+        Newlogin nl = new Newlogin();
+        nl.setVisible(true);
+
+    }
+    
+}
